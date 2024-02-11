9b4561511124d7203fecfb7b20574ef386745586
eziraa <ezratgab@gmail.com>
2024-02-11 11:56:28 +0300

    changing  the template to make it more nice ui

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 import urllib.request
+import json
 # Create your views here.
 
 
 def index(request):
-
-    return render(request, 'index.html')
+    # if request.method == 'POST':
+    #     city = request.POST['city']
+    #     res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
+    #                                  city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
+    #     json_data = json.loads(res)
+    return render(request, 'inde.html')
