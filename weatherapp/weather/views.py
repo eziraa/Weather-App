d0690872543bf150643fbd07908103c257dd1feb
eziraa <ezratgab@gmail.com>
2024-02-11 14:54:21 +0300

    fixing problem related to loading for first time rather than searching

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -8,19 +8,21 @@ from datetime import datetime
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        city = city.replace(' ', '%20')
-        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +
+    else:
+        city = "Bahir Dar"
+    city = city.replace(' ', '%20')
+    res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +
                                      city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
-        json_data = json.loads(res)
-        json_data['sys']['sunrise'] = datetime.utcfromtimestamp(
+    json_data = json.loads(res)
+    json_data['sys']['sunrise'] = datetime.utcfromtimestamp(
             json_data['sys']['sunrise']).strftime('%Y-%m-%d %H:%M:%S')
-        json_data['sys']['sunset'] = datetime.utcfromtimestamp(
+    json_data['sys']['sunset'] = datetime.utcfromtimestamp(
             json_data['sys']['sunset']).strftime('%Y-%m-%d %H:%M:%S')
 
 # Writing data to a JSON file
-        with open('formatted_data_with_time.json', 'w') as json_file:
+    with open('formatted_data_with_time.json', 'w') as json_file:
             json.dump(json_data, json_file, indent=4)
-        city = {
+    city = {
             'country': json_data['sys']['country'],
             'date': datetime.now(),
             'avg_temp': int(json_data['main']['temp']) - 273,
@@ -31,9 +33,7 @@ def index(request):
             'wind': json_data['wind']['speed'],
             'description': json_data['weather'][0]['description'],
         }
-        city['image'] = 'clear.jpg' if city['description'].__contains__(
+    city['image'] = 'clear.jpg' if city['description'].__contains__(
             'clear') else 'rain.jpg' if city['description'].__contains__('rain') else 'cloudy.jpg' if city['description'].__contains__('cloud') else 'drizzle.jpg' if city['description'].__contains__('drizzle') else 'snow.jpg' if city['description'].__contains__('snow') else "bg.jpg"
-        print(city)
-    else:
-        city = ''
+
     return render(request, 'index.html', {'city': city})
