070b83b3587d4d719c632de18e7bca7597dfac41
eziraa <ezratgab@gmail.com>
2024-02-11 13:02:55 +0300

    adding more style

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -5,9 +5,14 @@ import json
 
 
 def index(request):
-    # if request.method == 'POST':
-    #     city = request.POST['city']
-    #     res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' +
-    #                                  city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
-    #     json_data = json.loads(res)
-    return render(request, 'inde.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+        city = city.replace(' ', '%20')
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q=' +
+                                     city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
+        json_data = json.loads(res)
+        with open('data.json', 'w') as file:
+            json.dump(json_data, file, indent=4)
+    else:
+        json_data = ''
+    return render(request, 'inde.html', {'data': json_data})
